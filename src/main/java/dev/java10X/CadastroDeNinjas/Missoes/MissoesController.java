9acf415f8a383b44b77dfe24d3e83335e3033cb0
EzequieldeSR <ezequieldesr@gmail.com>
2025-06-26 22:47:32 -0300

    feat: Cria as rotas controller em Missoes

--- src/main/java/dev/java10X/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10X/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,28 @@
 package dev.java10X.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missoes listadas com sucesso!";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criada com sucesso!";
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public String atualizarMissao(){
+        return "Missao atualizada com sucesso!";
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public String deletarMissao(){
+        return "Missao deletada com sucesso!";
+    }
 }
